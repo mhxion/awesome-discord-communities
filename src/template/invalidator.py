a151d8cfb6004ba171f9ce4fb2d27d69c4d6fc60
Mahadi Xion <mhxions@gmail.com>
2021-01-26 10:10:41 -0600

    Move retry limit chunk to metadata, remove unnecessary formatting

--- src/template/invalidator.py
+++ src/template/invalidator.py
@@ -1,4 +1,3 @@
-import requests
 import json
 import re
 from pathlib import Path
@@ -23,22 +22,19 @@ class CheckInvalid:
                 codes = invite_pattern.findall(_)
                 if codes:
                     for c in codes:
-                        invite_codes[c[4]] = c[0].replace("<br>", " ")
+                        invite_codes[c[4]] = c[0]
 
         with open(self.dump, encoding="utf-8", newline="\n", mode="w+") as dump:
             json.dump(invite_codes, dump, indent=4, ensure_ascii=False)
 
-    def check_invites(self, sleep: int = 1):
+    def check_invites(self, sleep: int):
         invalid_codes = {}
         with open(self.dump, encoding="utf-8", newline="\n", mode="r") as d:
             invites = json.load(d)
             for k in invites.keys():
-                r = metadata.DiscordCommunityMetadata(invite_code=k, sleep=sleep).parse_data()
-                try:
-                    if not r:
-                        invalid_codes[k] = invites[k]
-                except KeyError:
-                    raise requests.exceptions.Timeout(f'Possible rate limit. Reply: {k} : {r}')
+                r = metadata.DiscordCommunityMetadata(invite_code=k, sleep=sleep)
+                if not r.parse_data():
+                    invalid_codes[k] = invites[k]
 
         with open(self.invalidated, encoding="utf-8", newline="\n", mode="w+") as i:
             json.dump(invalid_codes, i, indent=4, ensure_ascii=False)
